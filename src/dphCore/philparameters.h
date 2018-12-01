d9f9a3741a79e65fc52e18f75fc736b27d20abe2
Ivan Zhuravlev <izhuravlev27@gmail.com>
2018-12-01 13:52:36 +0300

    Added constructor to PhilParameters

--- src/dphCore/philparameters.h
+++ src/dphCore/philparameters.h
@@ -6,6 +6,14 @@ namespace dph
 {
 	struct PhilParameters
 	{
+		using namespace std::chrono_literals;
+		PhilParameters(int ord, std::chrono::milliseconds eatDur = 300ms, std::chrono::milliseconds thDur = 800ms) :
+			ordinalNumber(ord),
+			eatDuration(eatDur),
+			thinkDuration(thDur)
+			{}
+		
+		int ordinalNumber;
 		std::chrono::milliseconds eatDuration;
 		std::chrono::milliseconds thinkDuration;
 	}
