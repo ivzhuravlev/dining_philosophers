0581aa5a27d185c417234433e85214bd96a897e6
Ivan Zhuravlev <izhuravlev27@gmail.com>
2018-12-20 16:28:44 +0300

    Fix pointers

--- dphGui/src/dinnersettingswidget.cpp
+++ dphGui/src/dinnersettingswidget.cpp
@@ -11,7 +11,7 @@ DinnerSettingsWidget::DinnerSettingsWidget(DinnerSettings def, QWidget* parent) 
 	QWidget(parent)
 {
 	QLabel* numLbl = new QLabel(tr("Number of philosophers"), this);
-	QSpinBox* _numSpin = new QSpinBox(this);
+	_numSpin = new QSpinBox(this);
 	_numSpin->setMinimum(4);
 	_numSpin->setMaximum(8);
 	_numSpin->setValue(def.philNum);
@@ -21,7 +21,7 @@ DinnerSettingsWidget::DinnerSettingsWidget(DinnerSettings def, QWidget* parent) 
 	numLay->addWidget(_numSpin);
 
 	QLabel* eatLbl = new QLabel(tr("Time of eating, ms"), this);
-	QSpinBox* _eatSpin = new QSpinBox(this);
+	_eatSpin = new QSpinBox(this);
 	_eatSpin->setMinimum(10);
 	_eatSpin->setMaximum(10000);
 	_eatSpin->setValue(def.eatDur.count());
@@ -31,7 +31,7 @@ DinnerSettingsWidget::DinnerSettingsWidget(DinnerSettings def, QWidget* parent) 
 	eatLay->addWidget(_eatSpin);
 
 	QLabel* thinkLbl = new QLabel(tr("Time of thinking, ms"), this);
-	QSpinBox* _thinkSpin = new QSpinBox(this);
+	_thinkSpin = new QSpinBox(this);
 	_thinkSpin->setMinimum(10);
 	_thinkSpin->setMaximum(10000);
 	_thinkSpin->setValue(def.thinkDur.count());
