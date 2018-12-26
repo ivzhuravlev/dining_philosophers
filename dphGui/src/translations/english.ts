e5fa91e233fe2baa5ad4721de96c831a3a923a91
Ivan Zhuravlev <izhuravlev27@gmail.com>
2018-12-26 10:07:24 +0300

    Made translation to Russian

--- dphGui/src/translations/english.ts
+++ dphGui/src/translations/english.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
 <context>
     <name>DinnerSettingsWidget</name>
     <message>
         <location filename="../dinnersettingswidget.cpp" line="13"/>
         <source>Number of philosophers</source>
-        <translation type="unfinished"></translation>
+        <translation>Number of philosophers</translation>
     </message>
     <message>
         <location filename="../dinnersettingswidget.cpp" line="23"/>
         <source>Time of eating, ms</source>
-        <translation type="unfinished"></translation>
+        <translation>Time of eating, ms</translation>
     </message>
     <message>
         <location filename="../dinnersettingswidget.cpp" line="33"/>
         <source>Time of thinking, ms</source>
-        <translation type="unfinished"></translation>
+        <translation>Time of thinking, ms</translation>
     </message>
 </context>
 <context>
@@ -24,37 +24,27 @@
     <message>
         <location filename="../logwidget.cpp" line="18"/>
         <source>eats</source>
-        <translation type="unfinished"></translation>
+        <translation>eats</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="21"/>
         <source>thinks</source>
-        <translation type="unfinished"></translation>
+        <translation>thinks</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="24"/>
         <source>waits</source>
-        <translation type="unfinished"></translation>
+        <translation>waits</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="27"/>
         <source>finished</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../logwidget.cpp" line="33"/>
-        <source>%1 - %2 %3%4 %5</source>
-        <translation type="unfinished"></translation>
+        <translation>finished</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="34"/>
         <source>Philosopher</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../logwidget.cpp" line="35"/>
-        <source>#</source>
-        <translation type="unfinished"></translation>
+        <translation>Philosopher</translation>
     </message>
 </context>
 <context>
@@ -62,87 +52,87 @@
     <message>
         <location filename="../mainwindow.cpp" line="43"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Simple solution of classic &lt;br&gt;Dining Philosophers problem&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Simple solution of classic &lt;br&gt;Dining Philosophers problem&lt;br&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="46"/>
         <source>(c) Ivan Zhuravlev, 2018</source>
-        <translation type="unfinished"></translation>
+        <translation>(c) Ivan Zhuravlev, 2018</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="51"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>About Qt</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="70"/>
         <source>&amp;Actions</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Actions</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="71"/>
         <source>Actions</source>
-        <translation type="unfinished"></translation>
+        <translation>Actions</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="74"/>
         <source>&amp;Start Dinner</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Start Dinner</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="75"/>
         <source>Let the dinner begins</source>
-        <translation type="unfinished"></translation>
+        <translation>Let the dinner begins</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="79"/>
         <source>Sto&amp;p Dinner</source>
-        <translation type="unfinished"></translation>
+        <translation>Sto&amp;p Dinner</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="80"/>
         <source>Enough of this nonsense</source>
-        <translation type="unfinished"></translation>
+        <translation>Enough of this nonsense</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="84"/>
         <source>Se&amp;ttings...</source>
-        <translation type="unfinished"></translation>
+        <translation>Se&amp;ttings...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="85"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="92"/>
         <source>&amp;Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Exit</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="93"/>
         <source>Go away</source>
-        <translation type="unfinished"></translation>
+        <translation>Go away</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="97"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Help</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="98"/>
         <source>A&amp;bout...</source>
-        <translation type="unfinished"></translation>
+        <translation>A&amp;bout...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="101"/>
         <source>About &amp;Qt...</source>
-        <translation type="unfinished"></translation>
+        <translation>About &amp;Qt...</translation>
     </message>
 </context>
 <context>
@@ -150,22 +140,22 @@
     <message>
         <location filename="../scenesettingswidget.cpp" line="11"/>
         <source>Philosopher circle radius</source>
-        <translation type="unfinished"></translation>
+        <translation>Philosopher circle radius</translation>
     </message>
     <message>
         <location filename="../scenesettingswidget.cpp" line="21"/>
         <source>Table circle radius</source>
-        <translation type="unfinished"></translation>
+        <translation>Table circle radius</translation>
     </message>
     <message>
         <location filename="../scenesettingswidget.cpp" line="31"/>
         <source>Fork width</source>
-        <translation type="unfinished"></translation>
+        <translation>Fork width</translation>
     </message>
     <message>
         <location filename="../scenesettingswidget.cpp" line="41"/>
         <source>Fork height</source>
-        <translation type="unfinished"></translation>
+        <translation>Fork height</translation>
     </message>
 </context>
 <context>
@@ -173,37 +163,37 @@
     <message>
         <location filename="../settingsdialog.cpp" line="50"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="53"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancel</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="55"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Default</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="71"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="111"/>
         <source>Dinner settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Dinner settings</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="117"/>
         <source>Scene settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Scene settings</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="123"/>
         <source>Visual settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Visual settings</translation>
     </message>
 </context>
 <context>
@@ -211,33 +201,27 @@
     <message>
         <location filename="../visualsettingswidget.cpp" line="21"/>
         <source>Philosopher</source>
-        <translation type="unfinished"></translation>
+        <translation>Philosopher</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="22"/>
         <source>Fork</source>
-        <translation type="unfinished"></translation>
+        <translation>Fork</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="23"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Font</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="28"/>
         <source>Waiting</source>
-        <translation type="unfinished"></translation>
+        <translation>Waiting</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="38"/>
-        <location filename="../visualsettingswidget.cpp" line="62"/>
-        <location filename="../visualsettingswidget.cpp" line="84"/>
-        <location filename="../visualsettingswidget.cpp" line="106"/>
-        <location filename="../visualsettingswidget.cpp" line="132"/>
-        <location filename="../visualsettingswidget.cpp" line="156"/>
-        <location filename="../visualsettingswidget.cpp" line="177"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="42"/>
@@ -247,37 +231,37 @@
         <location filename="../visualsettingswidget.cpp" line="136"/>
         <location filename="../visualsettingswidget.cpp" line="160"/>
         <source>Select color</source>
-        <translation type="unfinished"></translation>
+        <translation>Select color</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="53"/>
         <source>Eating</source>
-        <translation type="unfinished"></translation>
+        <translation>Eating</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="75"/>
         <source>Thinking</source>
-        <translation type="unfinished"></translation>
+        <translation>Thinking</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="97"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Ready</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="123"/>
         <source>Available</source>
-        <translation type="unfinished"></translation>
+        <translation>Available</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="147"/>
         <source>Occupied</source>
-        <translation type="unfinished"></translation>
+        <translation>Occupied</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="181"/>
         <source>Select font</source>
-        <translation type="unfinished"></translation>
+        <translation>Select font</translation>
     </message>
 </context>
 </TS>
--- dphGui/src/translations/russian.ts
+++ dphGui/src/translations/russian.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
 <context>
     <name>DinnerSettingsWidget</name>
     <message>
         <location filename="../dinnersettingswidget.cpp" line="13"/>
         <source>Number of philosophers</source>
-        <translation type="unfinished"></translation>
+        <translation>Число философов</translation>
     </message>
     <message>
         <location filename="../dinnersettingswidget.cpp" line="23"/>
         <source>Time of eating, ms</source>
-        <translation type="unfinished"></translation>
+        <translation>Время обеда, мс</translation>
     </message>
     <message>
         <location filename="../dinnersettingswidget.cpp" line="33"/>
         <source>Time of thinking, ms</source>
-        <translation type="unfinished"></translation>
+        <translation>Время размышления, мс</translation>
     </message>
 </context>
 <context>
@@ -24,37 +24,27 @@
     <message>
         <location filename="../logwidget.cpp" line="18"/>
         <source>eats</source>
-        <translation type="unfinished"></translation>
+        <translation>ест</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="21"/>
         <source>thinks</source>
-        <translation type="unfinished"></translation>
+        <translation>думает</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="24"/>
         <source>waits</source>
-        <translation type="unfinished"></translation>
+        <translation>ожидает</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="27"/>
         <source>finished</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../logwidget.cpp" line="33"/>
-        <source>%1 - %2 %3%4 %5</source>
-        <translation type="unfinished"></translation>
+        <translation>закончил</translation>
     </message>
     <message>
         <location filename="../logwidget.cpp" line="34"/>
         <source>Philosopher</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../logwidget.cpp" line="35"/>
-        <source>#</source>
-        <translation type="unfinished"></translation>
+        <translation>Философ</translation>
     </message>
 </context>
 <context>
@@ -62,87 +52,87 @@
     <message>
         <location filename="../mainwindow.cpp" line="43"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>О программе</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Simple solution of classic &lt;br&gt;Dining Philosophers problem&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Решение классической проблемы &lt;br&gt;об Обедающих философах&lt;br&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="46"/>
         <source>(c) Ivan Zhuravlev, 2018</source>
-        <translation type="unfinished"></translation>
+        <translation>(с) Иван Журавлев, 2018</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="51"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>О Qt</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="70"/>
         <source>&amp;Actions</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Действия</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="71"/>
         <source>Actions</source>
-        <translation type="unfinished"></translation>
+        <translation>Действия</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="74"/>
         <source>&amp;Start Dinner</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Начать обед</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="75"/>
         <source>Let the dinner begins</source>
-        <translation type="unfinished"></translation>
+        <translation>Начать обед</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="79"/>
         <source>Sto&amp;p Dinner</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Остановить обед</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="80"/>
         <source>Enough of this nonsense</source>
-        <translation type="unfinished"></translation>
+        <translation>Закончить обед</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="84"/>
         <source>Se&amp;ttings...</source>
-        <translation type="unfinished"></translation>
+        <translation>Н&amp;астройки...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="85"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="92"/>
         <source>&amp;Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Выход</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="93"/>
         <source>Go away</source>
-        <translation type="unfinished"></translation>
+        <translation>Выйти из программы</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="97"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Справка</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="98"/>
         <source>A&amp;bout...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;О программе...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="101"/>
         <source>About &amp;Qt...</source>
-        <translation type="unfinished"></translation>
+        <translation>О &amp;Qt...</translation>
     </message>
 </context>
 <context>
@@ -150,22 +140,22 @@
     <message>
         <location filename="../scenesettingswidget.cpp" line="11"/>
         <source>Philosopher circle radius</source>
-        <translation type="unfinished"></translation>
+        <translation>Радиус кружка философа</translation>
     </message>
     <message>
         <location filename="../scenesettingswidget.cpp" line="21"/>
         <source>Table circle radius</source>
-        <translation type="unfinished"></translation>
+        <translation>Радиус стола</translation>
     </message>
     <message>
         <location filename="../scenesettingswidget.cpp" line="31"/>
         <source>Fork width</source>
-        <translation type="unfinished"></translation>
+        <translation>Ширина вилки</translation>
     </message>
     <message>
         <location filename="../scenesettingswidget.cpp" line="41"/>
         <source>Fork height</source>
-        <translation type="unfinished"></translation>
+        <translation>Длина вилки</translation>
     </message>
 </context>
 <context>
@@ -173,37 +163,37 @@
     <message>
         <location filename="../settingsdialog.cpp" line="50"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>ОК</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="53"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отмена</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="55"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>По умолчанию</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="71"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="111"/>
         <source>Dinner settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки обеда</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="117"/>
         <source>Scene settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки сцены</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="123"/>
         <source>Visual settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Визуальные настройки</translation>
     </message>
 </context>
 <context>
@@ -211,33 +201,22 @@
     <message>
         <location filename="../visualsettingswidget.cpp" line="21"/>
         <source>Philosopher</source>
-        <translation type="unfinished"></translation>
+        <translation>Философ</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="22"/>
         <source>Fork</source>
-        <translation type="unfinished"></translation>
+        <translation>Вилка</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="23"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Шрифт</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="28"/>
         <source>Waiting</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../visualsettingswidget.cpp" line="38"/>
-        <location filename="../visualsettingswidget.cpp" line="62"/>
-        <location filename="../visualsettingswidget.cpp" line="84"/>
-        <location filename="../visualsettingswidget.cpp" line="106"/>
-        <location filename="../visualsettingswidget.cpp" line="132"/>
-        <location filename="../visualsettingswidget.cpp" line="156"/>
-        <location filename="../visualsettingswidget.cpp" line="177"/>
-        <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ожидание</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="42"/>
@@ -247,37 +226,37 @@
         <location filename="../visualsettingswidget.cpp" line="136"/>
         <location filename="../visualsettingswidget.cpp" line="160"/>
         <source>Select color</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите цвет</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="53"/>
         <source>Eating</source>
-        <translation type="unfinished"></translation>
+        <translation>Обед</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="75"/>
         <source>Thinking</source>
-        <translation type="unfinished"></translation>
+        <translation>Размышление</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="97"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Готовность</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="123"/>
         <source>Available</source>
-        <translation type="unfinished"></translation>
+        <translation>Доступна</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="147"/>
         <source>Occupied</source>
-        <translation type="unfinished"></translation>
+        <translation>Занята</translation>
     </message>
     <message>
         <location filename="../visualsettingswidget.cpp" line="181"/>
         <source>Select font</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите шрифт</translation>
     </message>
 </context>
 </TS>
