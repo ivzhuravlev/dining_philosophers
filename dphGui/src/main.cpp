6e2e0085b1cb358c157d5fa52dbd455bc7b0d3ef
Ivan Zhuravlev <izhuravlev27@gmail.com>
2018-12-27 13:54:28 +0300

    Change application version

--- dphGui/src/main.cpp
+++ dphGui/src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 	QApplication app(argc, argv);
 	QCoreApplication::setOrganizationName("IZhuravlev");
 	QCoreApplication::setApplicationName("Dining Philosophers");
-	QCoreApplication::setApplicationVersion("0.2.0");
+	QCoreApplication::setApplicationVersion("1.0.0");
 
 	MainWindow w;
 	w.show();
