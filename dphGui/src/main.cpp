103e04f815d40b61c963f56e1bf6664307791a94
Ivan Zhuravlev <izhuravlev27@gmail.com>
2019-01-19 16:47:40 +0300

    Released version 1.1.0

--- dphGui/src/main.cpp
+++ dphGui/src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 	QApplication app(argc, argv);
 	QCoreApplication::setOrganizationName("IZhuravlev");
 	QCoreApplication::setApplicationName("Dining Philosophers");
-	QCoreApplication::setApplicationVersion("1.0.1");
+	QCoreApplication::setApplicationVersion("1.1.0");
 
 	MainWindow w;
 	w.show();
